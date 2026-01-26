3fe3edf863f16e167f90c25e621f862758215337
Michael Cutsail <mcutsail958@student.fuhsd.org>
2026-01-26 10:16:54 -0800

    Updated based on checkstyles

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,37 +1,17 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.KilogramSquareMeters;
-import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.Volts;
-
 import java.io.IOException;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.numbers.N8;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.Mass;
-import edu.wpi.first.units.measure.MomentOfInertia;
-import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.Filesystem;
 
 public class Constants {
-	public static final class DriveConstants {
+	public static final class DrivetrainConstants {
 		public static final int NUM_MODULES = 4;
 		public static final double SYS_ID_VOLT_DAMP = 6;
 
@@ -61,114 +41,6 @@ public class Constants {
 		public static final double STEER_S = 0;
 	}
 
-	public static final class AutoConstants {
-		public static final double TRANSLATION_P = 10;
-		public static final double TRANSLATION_I = 0;
-		public static final double TRANSLATION_D = 0;
-
-		public static final double ROTATION_P = 7;
-		public static final double ROTATION_I = 0;
-		public static final double ROTATION_D = 0;
-
-		public static final double POSE_TOLERANCE = 0.5;
-		public static final double TIME_STEP = 0.02;
-		public static final double MIN_VELOCITY_VEC = 0.1;
-
-		public static final Distance REEF_Y_LEFT_OFFSET = Inches.of(-10);
-		public static final Distance REEF_Y_RIGHT_OFFSET = Inches.of(2.5);
-
-		//TODO: These are just rough estimates: Ask what the drivers think is reasonable
-		public static final Distance STATION_FAR_LEFT_OFFSET = Inches.of(-20);
-		public static final Distance STATION_LEFT_OFFSET = Inches.of(-10);
-		public static final Distance STATION_CENTER_OFFSET = Inches.of(0);
-		public static final Distance STATION_FAR_RIGHT_OFFSET = Inches.of(10);
-		public static final Distance STATION_RIGHT_OFFSET = Inches.of(20);
-	}
-
-	public static final class SimConstants {
-		public static final Mass MASS_WITH_BUMPER = Pounds.of(115);
-		public static final Distance ROBOT_LENGTH = Inches.of(34.5);
-		public static final Distance ROBOT_WIDTH = Inches.of(34.5);
-		public static final double WHEEL_COF = 1.2;
-
-		public static final double STEER_P = 70;
-		public static final double STEER_I = 0;
-		public static final double STEER_D = 4.5;
-		public static final double STEER_S = 0;
-		public static final double STEER_V = 1.91;
-		public static final double STEER_A = 0;
-
-		public static final double STEER_MOTOR_GEAR_RATIO = 16.0;
-		public static final Voltage DRIVE_FRICTION_VOLTAGE = Volts.of(0.1);
-		public static final Voltage STEER_FRICTION_VOLTAGE = Volts.of(0.51);
-		public static final MomentOfInertia STEER_INERTIA = KilogramSquareMeters.of(0.05);
-
-		/* ================== PhotonSim Camera Properties ==================== */
-		public static final int CAM_FPS = 100;
-		public static final int CAM_RES_WIDTH_PIX = 640;
-		public static final int CAM_RES_HEIGHT_PIX = 480;
-
-
-		/* THE FOLLOWING CAMERA PROPERTIES ARE TAKEN FROM THE camprops.sqlite FILE */
-		public static final Matrix<N3, N3> REEF_CAMERA_CALIBRATION = new Matrix<>(
-			N3.instance, N3.instance,
-				new double[] {
-					554.8363329613238,
-					0.0,
-					319.771006175582,
-					0.0,
-					555.7640379607542,
-					210.90231168898111,
-					0.0,
-					0.0,
-					1.0
-				}
-			);
-
-		public static final Matrix<N8, N1> REEF_CAMERA_DISTORTION = new Matrix<>(
-			N8.instance, N1.instance,
-				new double[] {
-					0.032904169887820925,
-					0.024981667114235325,
-					-0.0024512685439365967,
-					9.347928373666906E-4,
-					-0.15993971100687385,
-					-2.8908154357146817E-4,
-					1.516375932970693E-4,
-					0.006735034604041476
-				}
-			);
-
-		public static final Matrix<N3, N3> STATION_CAMERA_CALIBRATION = new Matrix<>(
-			N3.instance, N3.instance,
-				new double[] {
-					548.8107781815636,
-					0.0,
-					335.98845208944647,
-					0.0,
-					549.91022315822,
-					261.5076314193876,
-					0.0,
-					0.0,
-					1.0
-				}
-			);
-
-		public static final Matrix<N8, N1> STATION_CAMERA_DISTORTION = new Matrix<>(
-			N8.instance, N1.instance,
-				new double[] {
-					0.046882076180144325,
-					-0.08739491623632688,
-					-7.369602850193537E-4,
-					9.49279422750342E-4,
-					0.015437967521711683,
-					-0.0018478126980591776,
-					0.004435053264404992,
-					-1.8178696218760975E-4
-				}
-			);
-	}
-
 	public static final class VisionConstants {
 
 		public static final AprilTagFieldLayout TAG_LAYOUT;
@@ -176,51 +48,28 @@ public class Constants {
 		static {
 			AprilTagFieldLayout layout;
 			try {
-				layout = Features.USE_TEST_FIELD && !Robot.isSimulation() ? new AprilTagFieldLayout(Filesystem.
-					getDeployDirectory() + "/gs-test-field.json") : AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
+				if (Features.USE_TEST_FIELD && !Robot.isSimulation()) {
+					layout = new AprilTagFieldLayout(
+						Filesystem.getDeployDirectory() + "/gs-test-field.json"
+					);
+				} else {
+					layout = AprilTagFieldLayout.loadField(
+						AprilTagFields.k2026RebuiltWelded
+					);
+				}
 			} catch (IOException e) {
-				System.out.println("Could not find test field, defaulting to reefscape welded field.");
-				layout = AprilTagFieldLayout
-				.loadField(AprilTagFields.k2025ReefscapeWelded);
+				System.out.println("Couldn't find test field, defaulting to reefscape welded field.");
+				layout = AprilTagFieldLayout.loadField(
+					AprilTagFields.k2026RebuiltWelded
+				);
 			}
 
-			var origin = new Pose3d(new Pose2d(-layout.getFieldLength() / 2, -layout.getFieldWidth() / 2, new Rotation2d()));
+			var origin = new Pose3d(new Pose2d(
+				-layout.getFieldLength() / 2, -layout.getFieldWidth() / 2, new Rotation2d()
+			));
 			layout.setOrigin(origin);
 			TAG_LAYOUT = layout;
 
 		}
-
-		public static final int TAG_ID_TEST_STATION = 1;
-		public static final int TAG_ID_TEST_REEF_LEFT = 2;
-		public static final int TAG_ID_TEST_REEF_RIGHT = 3;
-
-		public static final String REEF_CAMERA_NAME = "Reef_Camera";
-		public static final String STATION_CAMERA_NAME = "Source_Camera";
-
-		public static final Transform3d ROBOT_TO_REEF_CAM =
-			new Transform3d(Units.inchesToMeters(7.129),
-				-Units.inchesToMeters(4.306),
-				Units.inchesToMeters(14.56), new Rotation3d(0.0, 0.0, 0.0));
-		public static final Transform3d ROBOT_TO_STATION_CAM = new Transform3d(
-				-Units.inchesToMeters(8.875),
-				-Units.inchesToMeters(9.5),
-				Units.inchesToMeters(37.596), new Rotation3d(0.0, -Math.toRadians(19), Math.PI));
-
-		public static final double MAX_AMBIGUITY = 0.1;
-		public static final double MAX_Z_ERROR = 0.3; // meters
-		public static final Distance STOP_PATHFINDING_UPDATES = Meters.of(2);
-		public static final Angle MAX_POSE_ROT_OFFSET = Degrees.of(5);
-
-		public static final double LINEAR_STD_DEV_BASELINE = 0.02;
-		public static final double ANGULAR_STD_DEV_BASELINE = 0.06;
-
-		public static final double[] CAMERA_STD_DEV_FACTORS = new double[] {
-			1.0, // Reef Camera
-			1 // Station Camera
-		};
-
-		public static final double FIELD_BORDER_MARGIN = 0.5;
-		public static final double FIELD_LENGTH = 17.5483;
-		public static final double FIELD_WIDTH = 8.0519;
 	}
 }
--- src/main/java/frc/robot/FieldHelper.java
+++ src/main/java/frc/robot/FieldHelper.java
@@ -1,314 +1,261 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Meters;
 import static frc.robot.Constants.VisionConstants.TAG_LAYOUT;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.units.measure.Distance;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.SimConstants;
 
 /*
- * TOTAL # OF APRIL TAGS: 32
- * 
- * BLUE HUB APRIL TAGS (8 TOTAL): 25, 26, 27, 18, 19, 20, 21, 24
- * RED HUB APRIL TAGS (8 TOTAL): 9, 10, 11, 2, 3, 4, 5, 8
- * 
- * RIGHT SIDE BLUE TRENCH (2 TOTAL): 28 (INSIDE), 17 (OUTSIDE)
- * LEFT SIDE BLUE TRENCH (2 TOTAL): 23 (INSIDE), 22 (OUTSIDE)
- * RIGHT SIDE RED TRENCH (2 TOTAL): 12 (INSIDE), 1 (OUTSIDE)
- * LEFT SIDE RED TRENCH (2 TOTAL): 7 (INSIDE), 6 (OUTSIDE)
- * 
- * BLUE TOWER: 31 (CENTER), 32 (LEFT)
- * RED TOWER: 15 (CEMTER), 16 (LEFT)
- * 
- * BLUE DEPOT: 30 (LEFT), 29 (RIGHT)
- * RED DEPOT: 14 (LEFT), 13 (RIGHT)
- */
+* TOTAL # OF APRIL TAGS: 32
+*
+* BLUE HUB APRIL TAGS (8 TOTAL): 25, 26, 27, 18, 19, 20, 21, 24
+* RED HUB APRIL TAGS (8 TOTAL): 9, 10, 11, 2, 3, 4, 5, 8
+*
+* RIGHT SIDE BLUE TRENCH (2 TOTAL): 28 (INSIDE), 17 (OUTSIDE)
+* LEFT SIDE BLUE TRENCH (2 TOTAL): 23 (INSIDE), 22 (OUTSIDE)
+* RIGHT SIDE RED TRENCH (2 TOTAL): 12 (INSIDE), 1 (OUTSIDE)
+* LEFT SIDE RED TRENCH (2 TOTAL): 7 (INSIDE), 6 (OUTSIDE)
+*
+* BLUE TOWER: 31 (CENTER), 32 (LEFT)
+* RED TOWER: 15 (CENTER), 16 (LEFT)
+*
+* BLUE DEPOT: 30 (LEFT), 29 (RIGHT)
+* RED DEPOT: 14 (LEFT), 13 (RIGHT)
+*/
 
 public final /* singleton */ class FieldHelper {
 
-    // Going counter-clockwise to better understand (Front, Right, Back, Left)
-    public enum HubTag {
-        A, B, C, D, E, F, G, H
-    }
+	// Going counter-clockwise to better understand (Front, Right, Back, Left)
+	// Specific tag IDs for every april tag listed in HubTag
+	// This is for the blue side, subtract 16 from all tag IDs for red side...
 
-    private static Map<HubTag, AprilTag> hubAprilTags = new HashMap<>();
+	public enum HubTag {
+		A, B, C, D, E, F, G, H
+	}
 
-    // Specific tag IDs for every april tag listed in HubTag
-    // This is for the blue side, subtract 16 from all tag IDs for red side...
-    public static final int TAG_ID_HUB_SIDE_A = 25;
-    public static final int TAG_ID_HUB_SIDE_B = 26;
-    public static final int TAG_ID_HUB_SIDE_C = 27;
-    public static final int TAG_ID_HUB_SIDE_D = 18;
-    public static final int TAG_ID_HUB_SIDE_E = 19;
-    public static final int TAG_ID_HUB_SIDE_F = 20;
-    public static final int TAG_ID_HUB_SIDE_G = 21;
-    public static final int TAG_ID_HUB_SIDE_H = 24;
+	public enum HubSide {
+		NEUTRAL, ALLIANCE
+	}
 
-    // Designed for blue team, subtract 16 for red side
-    // Left inside is for the tag of the left trench on your alliance side and everything else should be self-explanatory
-    public enum TrenchTag {
-        LEFTIN, LEFTOUT, RIGHTIN, RIGHTOUT
-    }
+	public static final int TAGIDHUBSIDEA = 25;
+	public static final int TAGIDHUBSIDEB = 26;
+	public static final int TAGIDHUBSIDEC = 27;
+	public static final int TAGIDHUBSIDED = 18;
+	public static final int TAGIDHUBSIDEE = 19;
+	public static final int TAGIDHUBSIDEF = 20;
+	public static final int TAGIDHUBSIDEG = 21;
+	public static final int TAGIDHUBSIDEH = 24;
 
-    private static Map<TrenchTag, AprilTag> trenchAprilTags = new HashMap<>();
+	// Doing the same for towers...
+	// Designed for blue team, subtract 16 for red side
+	// Once again, designed for blue team...
+	public enum TowerTag {
+		CENTER, LEFT
+	}
 
-    public static final int TAG_ID_TRENCH_LEFT_IN = 7;
-    public static final int TAG_ID_TRENCH_LEFT_OUT = 6;
-    public static final int TAG_ID_TRENCH_RIGHT_IN = 12;
-    public static final int TAG_ID_TRENCH_RIGHT_OUT = 1;
+	public enum TowerSide {
+		CENTER, LEFT, RIGHT
+	}
 
-    // Once again, designed for blue team...
-    public enum TowerTag {
-        CENTER, LEFT
-    }
+	public static final int TAGIDTOWERCENTER = 31;
+	public static final int TAGIDTOWERLEFT = 32;
 
-    private static Map<TowerTag, AprilTag> towerAprilTags = new HashMap<>();
+	// List of starting positions
+	public enum StartingPose {
+		R1(RED1_STARTING_POSITION),
+		R2(RED2_STARTING_POSITION),
+		R3(RED3_STARTING_POSITION),
+		B1(BLUE1_STARTING_POSITION),
+		B2(BLUE2_STARTING_POSITION),
+		B3(BLUE3_STARTING_POSITION);
 
-    public static final int TAG_ID_TOWER_CENTER = 31;
-    public static final int TAG_ID_TOWER_LEFT = 32;
+		private final Pose2d pose;
 
-    public enum DepotTag {
-        LEFT, RIGHT
-    }
+		StartingPose(Pose2d thePose) {
+			pose = thePose;
+		}
 
-    private static Map<DepotTag, AprilTag> depotAprilTags = new HashMap<>();
+		/**
+		 * Gets the pose.
+		 *
+		 * @return the pose
+		 */
+		public Pose2d getPose() {
+			return pose;
+		}
+	}
 
-    public static final int TAG_ID_DEPOT_RIGHT = 29;
-    public static final int TAG_ID_DEPOT_LEFT = 30;
+	// Dimensions are 317.7 inches (width) by 651.2 inches (length)
+	// Divided 317.7 inches into quarters and put the robots on those lines for starting positions
+	// Alliance line is 158.6 inches into the field
+	// Backing up the robots by 5 inches from the starting line (can be adjusted later)
 
-    // List of starting positions
-    public enum StartingPose {
-        R1(RED1_STARTING_POSITION),
-        R2(RED2_STARTING_POSITION),
-        R3(RED3_STARTING_POSITION),
-        B1(BLUE1_STARTING_POSITION),
-        B2(BLUE2_STARTING_POSITION),
-        B3(BLUE3_STARTING_POSITION);
+	public static final Pose2d RED1_STARTING_POSITION = new Pose2d(
+		3.52044,
+		2.017395,
+		new Rotation2d(Math.PI));
 
-        private final Pose2d pose;
+	public static final Pose2d RED2_STARTING_POSITION = new Pose2d(
+		3.52044,
+		4.03479,
+		new Rotation2d(Math.PI));
 
-        StartingPose(Pose2d thePose) {
-            pose = thePose;
-        }
+	public static final Pose2d RED3_STARTING_POSITION = new Pose2d(
+		3.52044,
+		6.052185,
+		new Rotation2d(Math.PI));
 
-        /**
-         * Gets the pose.
-         *
-         * @return the pose
-         */
-        public Pose2d getPose() {
-            return pose;
-        }
-    }
+	public static final Pose2d BLUE1_STARTING_POSITION = new Pose2d(
+		13.02004,
+		2.017395,
+		new Rotation2d());
+	public static final Pose2d BLUE2_STARTING_POSITION = new Pose2d(
+		13.02004,
+		4.03479,
+		new Rotation2d());
+	public static final Pose2d BLUE3_STARTING_POSITION = new Pose2d(
+		13.02004,
+		6.052185,
+		new Rotation2d());
 
-    // Dimensions are 317.7 inches (width) by 651.2 inches (length)
-    // Divided 317.7 inches into quarters and put the robots on those lines for starting positions
-    // Alliance line is 158.6 inches into the field
-    // Backing up the robots by 5 inches from the starting line (can be adjusted later)
+	private static HashMap<HubTag, AprilTag> hubAprilTags;
+	private static HashMap<TowerTag, AprilTag> towerAprilTags;
+	// Adding all the HubTag IDs with their corresponding April Tags to the hubAprilTags map...
+	{
+		hubAprilTags = new HashMap<>(
+			Map.of(
+				HubTag.A,
+				new AprilTag(
+					TAGIDHUBSIDEA,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEA).orElseThrow()
+				),
+				HubTag.B,
+				new AprilTag(
+					TAGIDHUBSIDEB,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEB).orElseThrow(null)
+				),
+				HubTag.C,
+				new AprilTag(
+					TAGIDHUBSIDEC,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEC).orElseThrow(null)
+				),
+				HubTag.D,
+				new AprilTag(
+					TAGIDHUBSIDED,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDED).orElseThrow(null)
+				),
+				HubTag.E,
+				new AprilTag(
+					TAGIDHUBSIDEE,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEE).orElseThrow(null)
+				),
+				HubTag.F,
+				new AprilTag(
+					TAGIDHUBSIDEF,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEF).orElseThrow(null)
+				),
+				HubTag.G,
+				new AprilTag(
+					TAGIDHUBSIDEG,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEG).orElseThrow(null)
+				),
+				HubTag.H,
+				new AprilTag(
+					TAGIDHUBSIDEH,
+					TAG_LAYOUT.getTagPose(TAGIDHUBSIDEH).orElseThrow(null)
+				)
 
-    public static final Pose2d RED1_STARTING_POSITION = new Pose2d(
-            3.52044,
-            2.017395,
-            new Rotation2d(Math.PI));
+			)
+		);
 
-    public static final Pose2d RED2_STARTING_POSITION = new Pose2d(
-            3.52044,
-            4.03479,
-            new Rotation2d(Math.PI));
+		towerAprilTags = new HashMap<>(
+			Map.of(
+				TowerTag.LEFT,
+				new AprilTag(
+					TAGIDTOWERLEFT,
+					TAG_LAYOUT.getTagPose(TAGIDTOWERLEFT).orElseThrow(null)
+				),
+				TowerTag.CENTER,
+				new AprilTag(
+					TAGIDTOWERCENTER,
+					TAG_LAYOUT.getTagPose(TAGIDTOWERCENTER).orElseThrow(null)
+				)
+			)
+		);
+	}
 
-    public static final Pose2d RED3_STARTING_POSITION = new Pose2d(
-            3.52044,
-            6.052185,
-            new Rotation2d(Math.PI));
+	/**
+	 * Get a map of the hub april tags.
+	 *
+	 * @return a map of the hub april tags
+	 */
+	public static Map<HubTag, AprilTag> getHubTags() {
+		return hubAprilTags;
+	}
 
-    public static final Pose2d BLUE1_STARTING_POSITION = new Pose2d(
-            13.02004,
-            2.017395,
-            new Rotation2d());
-    public static final Pose2d BLUE2_STARTING_POSITION = new Pose2d(
-            13.02004,
-            13.02004,
-            new Rotation2d());
-    public static final Pose2d BLUE3_STARTING_POSITION = new Pose2d(
-            13.02004,
-            6.052185,
-            new Rotation2d());
+	/**
+	 * Get a map of the tower april tags.
+	 *
+	 * @return a map of the tower april tags
+	 */
+	public static Map<TowerTag, AprilTag> getTowerTags() {
+		return towerAprilTags;
+	}
 
-    // Adding all the HubTag IDs with their corresponding April Tags to the hubAprilTags map...
-    static {
-        hubAprilTags.put(
-            HubTag.A,
-            new AprilTag(TAG_ID_HUB_SIDE_A, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_A).orElse(null))
-        );
-        hubAprilTags.put(
-            HubTag.B,
-            new AprilTag(TAG_ID_HUB_SIDE_B, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_B).orElse(null))
-        );
+	private static final int LOCATION_1 = 1;
+	private static final int LOCATION_2 = 2;
+	private static final int LOCATION_3 = 3;
 
-        hubAprilTags.put(
-            HubTag.C,
-            new AprilTag(TAG_ID_HUB_SIDE_C, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_C).orElse(null))
-        );
+	/** Get the robot starting position based off alliance and location.
+	 *
+	 * @param alliance the alliance you are on
+	 * @param location starting location of the robot
+	 * @return Pose2d with the location of the robot
+	 */
+	public static Pose2d getStartingPose(Alliance alliance, int location) {
+		if (alliance == Alliance.Red) {
+			switch (location) {
+				case LOCATION_1:
+					return StartingPose.R1.getPose();
+				case LOCATION_2:
+					return StartingPose.R2.getPose();
+				case LOCATION_3:
+					return StartingPose.R3.getPose();
+				default:
+					throw new IllegalArgumentException("Invalid location: " + location);
+			}
+		} else {
+			switch (location) {
+				case LOCATION_1:
+					return StartingPose.B1.getPose();
+				case LOCATION_2:
+					return StartingPose.B2.getPose();
+				case LOCATION_3:
+					return StartingPose.B3.getPose();
+				default:
+					throw new IllegalArgumentException("Invalid location: " + location);
+			}
+		}
+	}
 
-        hubAprilTags.put(
-            HubTag.D,
-            new AprilTag(TAG_ID_HUB_SIDE_D, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_D).orElse(null))
-        );
+	/**
+	 * Gets the desired pose to align to for the hub.
+	 * @param hubSide the side of the hub that the robot is on
+	 */
+	public static void getAlignedDesiredPoseForHub(HubSide hubSide) {
+		//TODO: Add functioning code here
+		throw new UnsupportedOperationException();
+	}
 
-        hubAprilTags.put(
-            HubTag.E,
-            new AprilTag(TAG_ID_HUB_SIDE_E, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_E).orElse(null))
-        );
-        hubAprilTags.put(
-            HubTag.F,
-            new AprilTag(TAG_ID_HUB_SIDE_F, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_F).orElse(null))
-        );
-
-        hubAprilTags.put(
-            HubTag.G,
-            new AprilTag(TAG_ID_HUB_SIDE_G, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_G).orElse(null))
-        );
-        
-        hubAprilTags.put(
-            HubTag.H,
-            new AprilTag(TAG_ID_HUB_SIDE_H, TAG_LAYOUT.getTagPose(TAG_ID_HUB_SIDE_H).orElse(null))
-        );
-    }
-
-    // Doing the same with trenches...
-    static {
-        trenchAprilTags.put(
-            TrenchTag.LEFTIN,
-            new AprilTag(TAG_ID_TRENCH_LEFT_IN, TAG_LAYOUT.getTagPose(TAG_ID_TRENCH_LEFT_IN).orElse(null))
-        );
-        trenchAprilTags.put(
-            TrenchTag.LEFTOUT,
-            new AprilTag(TAG_ID_TRENCH_LEFT_OUT, TAG_LAYOUT.getTagPose(TAG_ID_TRENCH_LEFT_OUT).orElse(null))
-        );
-
-        trenchAprilTags.put(
-            TrenchTag.RIGHTIN,
-            new AprilTag(TAG_ID_TRENCH_RIGHT_IN, TAG_LAYOUT.getTagPose(TAG_ID_TRENCH_RIGHT_IN).orElse(null))
-        );
-
-        trenchAprilTags.put(
-            TrenchTag.RIGHTOUT,
-            new AprilTag(TAG_ID_TRENCH_RIGHT_OUT, TAG_LAYOUT.getTagPose(TAG_ID_TRENCH_RIGHT_OUT).orElse(null))
-        );
-    }
-
-    // And depots + towers...
-    static {
-        towerAprilTags.put(
-            TowerTag.LEFT,
-            new AprilTag(TAG_ID_TOWER_LEFT, TAG_LAYOUT.getTagPose(TAG_ID_TOWER_LEFT).orElse(null))
-        );
-        towerAprilTags.put(
-            TowerTag.CENTER,
-            new AprilTag(TAG_ID_TOWER_CENTER, TAG_LAYOUT.getTagPose(TAG_ID_TOWER_CENTER).orElse(null))
-        );
-
-        depotAprilTags.put(
-            DepotTag.RIGHT,
-            new AprilTag(TAG_ID_DEPOT_RIGHT, TAG_LAYOUT.getTagPose(TAG_ID_DEPOT_RIGHT).orElse(null))
-        );
-
-        depotAprilTags.put(
-            DepotTag.LEFT,
-            new AprilTag(TAG_ID_DEPOT_LEFT, TAG_LAYOUT.getTagPose(TAG_ID_DEPOT_LEFT).orElse(null))
-        );
-    }
-
-    /**
-     * Get a map of the hub april tags.
-     *
-     * @return a map of the hub april tags
-     */
-    public static Map<HubTag, AprilTag> getHubTags() {
-        return hubAprilTags;
-    }
-
-    /**
-     * Get a map of the trench april tags.
-     *
-     * @return a map of the trench april tags
-     */
-    public static Map<TrenchTag, AprilTag> getTrenchTags() {
-        return trenchAprilTags;
-    }
-
-    /**
-     * Get a map of the tower april tags.
-     *
-     * @return a map of the tower april tags
-     */
-    public static Map<TowerTag, AprilTag> getTowerTags() {
-        return towerAprilTags;
-    }
-
-    /**
-     * Get a map of the depot april tags.
-     *
-     * @return a map of the depot april tags
-     */
-    public static Map<DepotTag, AprilTag> getDepotTags() {
-        return depotAprilTags;
-    }
-
-    /**
-     * Get the robot starting position based off alliance and location
-     * 
-     * @param alliance the alliance you are on
-     * @param location location of the robot 
-     */
-    public static Pose2d getStartingPose(Alliance alliance, int location) {
-        if (alliance == Alliance.Red) {
-            switch (location) {
-                case 1:
-                    return StartingPose.R1.getPose();
-                case 2:
-                    return StartingPose.R2.getPose();
-                case 3:
-                    return StartingPose.R3.getPose();
-                default:
-                    throw new IllegalArgumentException("Invalid location: " + location);
-            }
-        } else {
-            switch (location) {
-                case 1:
-                    return StartingPose.B1.getPose();
-                case 2:
-                    return StartingPose.B2.getPose();
-                case 3:
-                    return StartingPose.B3.getPose();
-                default:
-                    throw new IllegalArgumentException("Invalid location: " + location);
-            }
-        }
-    }
-
-    public static void getAlignedDesiredPoseForHub() {
-        //TODO: Add functioning code here
-    }
-
-    public static void getAlignedDesiredPoseForTrench() {
-        //TODO: Add functioning code here
-    }
-
-    public static void getAlignedDesiredPoseForTower() {
-        //TODO: Add functioning code here
-    }
-
-    public static void getAlignedDesiredPoseForDepot() {
-        //TODO: Add functioning code here
-    }   
+	/**
+	 * Gets the desired pose to align to for the tower.
+	 * @param towerSide the side of the tower that the robot is on
+	 */
+	public static void getAlignedDesiredPoseForTower(TowerSide towerSide) {
+		//TODO: Add functioning code here
+		throw new UnsupportedOperationException();
+	}
 }
