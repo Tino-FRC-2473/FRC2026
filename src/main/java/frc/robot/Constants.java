1c181b4974c2ec83854550d609ce4c26b0b8678c
Aditya Suresh <216782266+adityanexeng@users.noreply.github.com>
2026-01-26 01:05:53 -0800

    (chore) Add empty method functions and some constants, debug minor issues

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,72 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class Constants {
+	public static final class DrivetrainConstants {
+		public static final int NUM_MODULES = 4;
+		public static final double SYS_ID_VOLT_DAMP = 6;
+
+		public static final double TRANSLATION_DEADBAND = 0.1;
+		public static final double ROTATION_DEADBAND = 0.1;
+		public static final AngularVelocity MAX_ANGULAR_VELO_RPS = RotationsPerSecond.of(0.75);
+
+		//Set to the decimal corresponding to the percentage of how fast you want the bot to go
+		// 1 = 100% speed, 0.5 = 50% speed, 0.3 = 30% speed, and so on
+		public static final double TRANSLATIONAL_DAMP = 1;
+		public static final double ROTATIONAL_DAMP = 1;
+	}
+
+	public static final class ModuleConstants {
+		public static final double DRIVE_P = 0.1;
+		public static final double DRIVE_I = 0;
+		public static final double DRIVE_D = 0;
+		public static final double DRIVE_V = 0.124;
+
+		public static final double DRIVE_CURRENT_LIMIT = 60;
+		public static final double STEER_CURRENT_LIMIT = 60;
+
+		public static final double STEER_P = 100;
+		public static final double STEER_I = 0;
+		public static final double STEER_D = 0.5;
+		public static final double STEER_V = 0.1;
+		public static final double STEER_S = 0;
+	}
+    
+    public static final class ShooterConstants {
+        public static final Pose2d OUTPOST_POSE = new Pose2d();
+		public static final Pose2d TARGET3_POSE = new Pose2d();
+		public static final Pose2d HUB_POSE = new Pose2d();
+		
+		public static final double HOOD_GEAR_RATIO = 2;
+		public static final double FLYWHEEL_GEAR_RATIO = 3;
+		public static final double INDEXER_GEAR_RATIO = 3;
+
+		public static final int UPDATE_FREQUENCY_HZ = 200;
+		public static final double FLYWHEEL_ACCELERATION = 160;
+		public static final double FLYWHEEL_JERK = 10 * FLYWHEEL_ACCELERATION; 
+		public static final double HOOD_VELOCITY = 20;
+		public static final double HOOD_ACCELERATION = 2 * HOOD_VELOCITY;
+		public static final double HOOD_JERK = 10 * HOOD_ACCELERATION; 
+
+		public static final double FLYWHEEL_MOE = 0.5; //margin of error, subject to change
+		public static final double HOOD_MOE = 0.1; //margin of error, subject to change
+
+		public static final double FLYWHEEL_MAX_SPEED = 160; //rotations/second, temporary/placeholder
+
+		public static final double FLYWHEEL_MM_CONSTANT_S = 0.1; //need to test by recording small amount of input that allows any movement at all
+		public static final double HOOD_MM_CONSTANT_S = 0.1; //need to test by recording in some manner
+
+		//All of these are placeholder values, all need to be changed
+		public static final double FLYWHEEL_MM_CONSTANT_P = 0;
+		public static final double FLYWHEEL_MM_CONSTANT_I = 0;
+		public static final double FLYWHEEL_MM_CONSTANT_D = 0;
+		public static final double HOOD_MM_CONSTANT_P = 0;
+		public static final double HOOD_MM_CONSTANT_I = 0;
+		public static final double HOOD_MM_CONSTANT_D = 0;
+        
+    }
+}
--- src/main/java/frc/robot/TeleopInput.java
+++ src/main/java/frc/robot/TeleopInput.java
@@ -83,6 +83,25 @@ public class TeleopInput {
 		return rightJoystick.getY();
 	}
 
+	/* ========================== Triggers =============================*/
+	public boolean isLeftBumperPressed(){
+		return false;
+	}
+	public boolean isLeftTriggerPressed(){
+		return false;
+	}
+	public boolean isRightBumperPressed(){
+		return false;
+	}
+	public boolean isRightTriggerPressed(){
+		return false;
+	}
+	/*============================= Touchpad ========================== */
+	public boolean isTouchpadPressed(){
+		return false;
+	}
+
+
 	/* ======================== Private methods ======================== */
 
 }
