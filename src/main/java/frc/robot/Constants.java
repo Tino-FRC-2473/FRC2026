73582dad5b3a4004a75e6e0f69e832b690f3f88c
asuresh952 <asuresh952@student.fuhsd.org>
2026-02-28 13:57:52 -0800

    (feat) Hardcoded Shooter Prep Value

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -120,7 +120,7 @@ public class Constants {
 
 		public static final double SHOOTER_CURRENT_LIMIT = 40; //Amps
 
-		public static final double TEMP_FLYSPEED = 2;
+		public static final double TEMP_FLYSPEED = 55;
 
 		private static final double JERK_MULT_CONSTANT = 100;
 		//constant to change the magnitude of jerk from acceleration
