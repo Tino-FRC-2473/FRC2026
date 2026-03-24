d377f028c12d7e785a790fb3e58e6cb06117d8f2
fireninjaminer <fireninja.miner@gmail.com>
2026-03-23 17:17:06 -0700

    (feat) deleted ExampleFSMSystem and all references to it, other two FSMs and all references were already gone.

--- src/main/java/frc/robot/systems/AutoHandlerSystem.java
+++ src/main/java/frc/robot/systems/AutoHandlerSystem.java
@@ -22,9 +22,6 @@ public class AutoHandlerSystem {
 	private int currentStateIndex;
 
 	//FSM Systems that the autoHandlerFSM uses
-	private ExampleFSMSystem subsystem1;
-	private ExampleFSMSystem subsystem2;
-	private ExampleFSMSystem subsystem3;
 
 	//Predefined auto paths
 	private static final AutoFSMState[] PATH1 = new AutoFSMState[]{
@@ -39,15 +36,8 @@ public class AutoHandlerSystem {
 	/**
 	 * Create FSMSystem and initialize to starting state.
 	 * Initializes any subsystems such as driveFSM, armFSM, ect.
-	 * @param fsm1 the first subsystem that the auto handler will call functions on
-	 * @param fsm2 the second subsystem that the auto handler will call functions on
-	 * @param fsm3 the third subsystem that the auto handler will call functions on
+	 * Handles autonomous state transitions
 	 */
-	public AutoHandlerSystem(ExampleFSMSystem fsm1, ExampleFSMSystem fsm2, ExampleFSMSystem fsm3) {
-		subsystem1 = fsm1;
-		subsystem2 = fsm2;
-		subsystem3 = fsm3;
-	}
 
 	/* ======================== Public methods ======================== */
 	/**
@@ -68,9 +58,6 @@ public class AutoHandlerSystem {
 	 * @param path the auto path to be executed
 	 */
 	public void reset(AutoPath path) {
-		subsystem1.reset();
-		subsystem2.reset();
-		subsystem3.reset();
 
 		currentStateIndex = 0;
 		if (path == AutoPath.PATH1) {
@@ -94,19 +81,13 @@ public class AutoHandlerSystem {
 		System.out.println("In State: " + getCurrentState());
 		switch (getCurrentState()) {
 			case STATE1:
-				isCurrentStateFinished = subsystem1.updateAutonomous(AutoFSMState.STATE1)
-					&& subsystem2.updateAutonomous(AutoFSMState.STATE1)
-					&& subsystem3.updateAutonomous(AutoFSMState.STATE1);
+				isCurrentStateFinished = true;
 				break;
 			case STATE2:
-				isCurrentStateFinished = subsystem1.updateAutonomous(AutoFSMState.STATE2)
-					&& subsystem2.updateAutonomous(AutoFSMState.STATE2)
-					&& subsystem3.updateAutonomous(AutoFSMState.STATE2);
+				isCurrentStateFinished = true;
 				break;
 			case STATE3:
-				isCurrentStateFinished = subsystem1.updateAutonomous(AutoFSMState.STATE3)
-					&& subsystem2.updateAutonomous(AutoFSMState.STATE3)
-					&& subsystem3.updateAutonomous(AutoFSMState.STATE3);
+				isCurrentStateFinished = true;
 				break;
 			default:
 				throw new IllegalStateException("Invalid state: " + getCurrentState().toString());
